eb5249a1a06c8139e59e753519a4835c2246be0e
samamoazemi <moazemisama@gmail.com>
2022-01-16 01:53:44 -0800

    Manage form states

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,18 +1,46 @@
+import { useState } from "react";
+
 const SignUpForm = () => {
+
+    const [userData, setUserData] = useState({
+         name:"",
+         email:"",
+         password:"",
+    })
+
+        const changeHandler = (e) => {
+            setUserData({...userData, [e.target.name]: e.target.value });
+        }
+        
     return ( 
         <div className="mainSection">
             <form>
                 <div className="formControl">
                     <label>Nama</label>
-                    <input type="text"/>
+                    <input 
+                      type="text"
+                      onChange={changeHandler} 
+                      value={userData.name}
+                      name="name"
+                      />
                 </div>
                 <div className="formControl">
                     <label>Email</label>
-                    <input type="text"/>
+                    <input 
+                      type="text"
+                      onChange={changeHandler} 
+                      value={userData.email}
+                      name="email"
+                      />
                 </div>
                 <div className="formControl">
                     <label>Password</label>
-                    <input type="text"/>
+                    <input
+                      type="text"
+                      onChange={changeHandler} 
+                      value={userData.password}
+                      name="password"
+                      />
                 </div>
                 <button>submit</button>
             </form>
