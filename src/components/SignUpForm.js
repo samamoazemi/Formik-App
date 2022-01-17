9575498b722dd2631b648380554690c937ad7710
samamoazemi <moazemisama@gmail.com>
2022-01-17 04:31:28 -0800

    Optimize formik features

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -36,13 +36,7 @@ const SignUpForm = () => {
             <form onSubmit={formik.handleSubmit}>
                 <div className="formControl">
                     <label>Nama</label>
-                    <input 
-                      type="text"
-                      onChange={formik.handleChange} 
-                      onBlur={formik.handleBlur}
-                      value={formik.values.name}
-                      name="name"
-                      />
+                    <input type="text" {...formik.getFieldProps("name")} name="name"/>
                       {formik.errors.name && formik.touched.name && (
                       <div className="error">{formik.errors.name}</div>
                       )}
@@ -50,13 +44,7 @@ const SignUpForm = () => {
 
                 <div className="formControl">
                     <label>Email</label>
-                    <input 
-                      type="text"
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur} 
-                      value={formik.values.email}
-                      name="email"
-                      />
+                    <input type="text" {...formik.getFieldProps("email")} name="email"/>
                        {formik.errors.email && formik.touched.email && (
                       <div className="error">{formik.errors.email}</div>
                       )}
@@ -64,13 +52,7 @@ const SignUpForm = () => {
 
                 <div className="formControl">
                     <label>Password</label>
-                    <input
-                      type="text"
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur} 
-                      value={formik.values.password}
-                      name="password"
-                      />
+                    <input type="text" {...formik.getFieldProps("password")} name="password"/>
                        {formik.errors.password && formik.touched.password && (
                       <div className="error">{formik.errors.password}</div>
                       )}
